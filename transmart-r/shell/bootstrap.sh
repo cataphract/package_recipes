f67235e3bf58745af418fc6f93e2610d52871462
Gustavo Lopes <glopes@nebm.ist.utl.pt>
2014-08-06 15:42:34 +0200

    run apt-get update; ws

--- transmart-r/shell/bootstrap.sh
+++ transmart-r/shell/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 if [[ ! -d /etc/puppet/modules/vcsrepo ]]; then
-	if command -v apt-get; then
-		apt-get install -y puppet
-	else
-		yum install -y puppet
-	fi
+  if command -v apt-get; then
+    apt-get update
+    apt-get install -y puppet
+  else
+    yum install -y puppet
+  fi
     puppet module install puppetlabs-vcsrepo --version 1.0.1
 fi
